09301c21ae82284d88012afa5722f6950c747f1a
Jayden Fausto <joeyfausto107@gmail.com>
2025-01-04 00:04:14 -0800

    Pieces, Board, GameState I added starting code for each piece, board set up, and a class that stores the state of the board including pieces and current player turn.

---
+++ ChessLogic/Pieces/Bishop.cs
@@ -0,0 +1,15 @@
+﻿using ChessLogic.Pieces;
+
+namespace ChessLogic.Pieces;
+
+public class Bishop(Player color) : Piece
+{
+    public override PieceType Type => PieceType.Bishop;
+    public override Player Color { get; } = color;
+
+    public override Piece Copy() =>
+        new Bishop(Color)
+        {
+            HasMoved = HasMoved
+        };
+}
---
+++ ChessLogic/Pieces/Knight.cs
@@ -0,0 +1,13 @@
+﻿namespace ChessLogic.Pieces;
+
+public class Knight(Player color) : Piece
+{
+    public override PieceType Type => PieceType.Knight;
+    public override Player Color { get; } = color;
+
+    public override Piece Copy() =>
+        new Knight(Color)
+        {
+            HasMoved = HasMoved
+        };
+}
